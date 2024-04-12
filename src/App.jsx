2aeb5ff38208ac0b96840bcd17fa2ce34917f83f
yadavsatyapal1999 <114231518+yadavsatyapal1999@users.noreply.github.com>
2024-04-12 22:57:59 +0530

    First commit react project created with vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Navbar from './Navbar/Navbar'
+
+function App() {
+  
+
+  return (
+    <div>
+  <Navbar/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+
+
+
+
+export default function Navbar(){
+    
+
+    return <div id="outerNav">
+        <div>
+            <div>Navbar</div>
+            <div>
+            <div> <input type="text" />  </div>
+            <div>  <button>Search</button> </div>
+            </div>
+            <div>Cart</div>
+        </div>
+        <div></div>
+    </div>
+}
